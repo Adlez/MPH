037975e977aac250e2fed10e44f547177685a018
Adlez <srgnt_booyaw@live.ca>
2015-03-30 16:15:44 -0400

    Made WorldClass Better
    also added a teeny bit to Globals

--- Website/js/SpaceColonization/Globals.js
+++ Website/js/SpaceColonization/Globals.js
@@ -2,3 +2,8 @@
 var POWER_A = 30.0;
 var POWER_V = 10.0;
 var POWER_T = 20.0;
+
+//World Static Variables
+var WORLD_TYPE_PLANET = 1;
+var WORLD_TYPE_SATELLITE = 2;
+var WORLD_TYPE_ASTEROID = 3;
--- Website/js/SpaceColonization/WorldClass.js
+++ Website/js/SpaceColonization/WorldClass.js
@@ -1,11 +1,14 @@
 ﻿var WorldClass =
 	{
-		
-
-
-		h_WorldFertilityLevel: 0,
-		h_WorldMinerlaLevel: 0,
+		h_WorldType: 0,
 		h_WorldHasRuins: false,
+		h_WorldFertilityLevel: 0, //1:20%, 2:50%,3:20%,4:10%
+		h_WorldMineralLevel: 0, //1:45%, 2:25%, 3:25%, 4:5%
+		h_WorldHasRuins: false, //false:80%, true:20%
+		h_DistanceFromMainColony: 0.0,
+		h_DistanceFromCapital: 0.0,
+		h_WorldHasColony: false,
+
 		x: 0,
 		y: 0,
 		vX: 0,
@@ -14,7 +17,8 @@
 		width: undefined,
 		height: undefined,
 
-		init: function ( x, y, vX, vY, spriteAnim, width, height )
+		init: function ( x, y, vX, vY, spriteAnim, width, height, worldType, hasRuins, fLevel, mLevel,
+			distanceFromMainColony, distanceFromCapital, hasColony )
 		{
 			this.x = x;
 			this.y = y;
@@ -23,7 +27,26 @@
 			this.spriteAnim = spriteAnim;
 			this.width = width;
 			this.height = height;
+			this.h_WorldType = worldType;
+			this.h_WorldHasRuins = hasRuins;
+			this.h_WorldFertilityLevel = fLevel;
+			this.h_WorldMineralLevel = mLevel;
+			this.h_DistanceFromMainColony = distanceFromMainColony;
+			this.h_DistanceFromCapital = distanceFromCapital;
+			this.h_WorldHasColony = hasColony;
+
+			if(this.h_WorldType == 3)
+			{
+				this.h_WorldFertilityLevel = 0;
+				this.h_WorldMineralLevel += 2;
+			}
+			if(this.h_WorldType == 1)
+			{
+				this.h_WorldFertilityLevel += 1;
+			}
 		},
+
+
 
 	//	MainColonyWorld : WorldClass;
 	};
