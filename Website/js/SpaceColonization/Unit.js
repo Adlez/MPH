779fffe630e1e21d59d4ce7b80b40c3bddf77dd8
BrendanM <danmatrix@live.com>
2015-03-30 16:18:15 -0400

    unitz

--- Website/js/SpaceColonization/Unit.js
+++ Website/js/SpaceColonization/Unit.js
@@ -1,39 +1,38 @@
 // JavaScript source code
-var Unit;
-
-Unit.Velos;
-Unit.Aegis;
-Unit.Titav;
-
-Unit.init = function(POWER_A, POWER_T, POWER_V)
+var UnitClass = Object.create(GameObjectClass);
 {
-    this.Velos = POWER_V;
-    this.Aegis = POWER_A;
-    this.Titav = POWER_T;
-}
+    UnitClass.Velos;
+    UnitClass.Titav;
+    UintClass.Aegis;
 
-function CreateUnits() {
-
-    var Units = [];
-
-    //if() get when button is pressed to make a unit
-    Units[Units.length] = Unit.Aegis;
-    //
-    Units[Units.length] = Unit.Titav;
-    //
-    Units[Units.length] = Unit.Velos;
-
-}
-
-function RemoveUnits() {
-
-    var index;
-
-    for (index = 0; Units.length < index; index++)
+    UnitClass.init = function(POWER_A, POWER_T, POWER_V)
     {
-        if (Units[0] == POWER_V)
+        this.Velos = POWER_V;
+        this.Aegis = POWER_A;
+        this.Titav = POWER_T;
+    };
+
+    function CreateUnits() {
+
+        var Units = new Array();
+
+        //if() get when button is pressed to make a unit
+        Units.push(Velos);
+        //
+        Units.push(Titav);
+        //
+        Units[Units.length] = Unit.Velos;
+
+    }
+
+    function RemoveUnits() {
+
+        for (var index = 0; index < this.Units.length; index++)
         {
-            Units.slice(0);
+            if (Units[0] == POWER_V)
+            {
+                Units.slice(0);
+            }
         }
     }
 }
